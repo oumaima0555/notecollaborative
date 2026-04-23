60db4f57fbefc014ae0d506fea07386a20f5593a
aichabcm-lab <aichabcm@gmail.com>
2026-04-23 17:32:38 +0100

    model utilisateur note

--- notecollabrative/notecollabrative/settings.py
+++ notecollabrative/notecollabrative/settings.py
@@ -37,9 +37,11 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'administrateur',
-    'collaborateur',
     'utilisateur',
+    'note',
+    'tag',
+    'commentaire',
+    'notification',
 ]
 
 MIDDLEWARE = [
--- notecollabrative/utilisateur/models.py
+++ notecollabrative/utilisateur/models.py
@@ -5,13 +5,44 @@ from django.contrib.auth.models import AbstractUser,Group,Permission
 #voici les models 
 
 class Utilisateur(AbstractUser):
-    # Ajoutez des champs personnalisés si nécessaire
-    pass
+    id=models.AutoField(primary_key=True)
+    nom=models.CharField(max_length=100)
+    prenom=models.CharField(max_length=100)
+    num=models.CharField(max_length=20)
+    date_inscription=models.DateTimeField(auto_now_add=True)
+    class Meta:
+        verbose_name = 'Utilisateur'
+        verbose_name_plural = 'Utilisateurs'
 
+    groups=models.ManyToManyField(
+        Group, 
+        related_name='utilisateurs',
+        related_query_name='utilisateur',
+        blank=True,
+        help_text='the groups this user belongs to. A user will get all permissions granted to each of their groups.'
+        )
+    user_permissions=models.ManyToManyField(
+        Permission,
+        related_name='utilisateurs',
+        related_query_name='utilisateur',
+        blank=True,
+        help_text='specific permissions for this user.'
+        )
+    
+    def __str__(self):
+        return f"{self.prenom} {self.nom}"
+    
 class Administrateur(Utilisateur):
     # Ajoutez des champs spécifiques à l'administrateur si nécessaire
-    pass
+    def supprimer_utilisateur(self,utilisateur):
+        utilisateur.delete()
+
+    def modifier_utilisateur(self,utilisateur):
+        # Implémentez la logique pour modifier les informations de l'utilisateur
+        pass
 
 class Collaborateur(Utilisateur):
     # Ajoutez des champs spécifiques au collaborateur si nécessaire
-    pass
+    def __str__(self):
+        return super().__str__()
+    
