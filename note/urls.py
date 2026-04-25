f20bbe32d8ab62ca658e725cf36ad62826768379
mabchoursalma4-spec <mabchoursalma4@gmail.com>
2026-04-25 12:51:09 +0100

    nouvelle version

--- note/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-from .views import note_list, note_detail, note_create
-
-urlpatterns = [
-    path('', note_list, name='note_list'),
-    path('create/', note_create, name='note_create'),
-    path('<int:pk>/', note_detail, name='note_detail'),
-]
